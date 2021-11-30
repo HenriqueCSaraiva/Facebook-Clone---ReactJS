6eda807e7845a4dad84e2bdc5edd782087d0f6b4
Henrique Saraiva <henriquesaraiva24@gmail.com>
2021-11-30 08:28:45 -0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAO6yp2gky1qN2IZjfBJbvVrsR7ZTHHNVI",
+    authDomain: "facebook-react-8ece3.firebaseapp.com",
+    projectId: "facebook-react-8ece3",
+    storageBucket: "facebook-react-8ece3.appspot.com",
+    messagingSenderId: "998186095317",
+    appId: "1:998186095317:web:531c7cedf5be5dcf37485c",
+    measurementId: "G-Y85B2P3K5Q"
+  };
