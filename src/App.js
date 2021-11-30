3b4af09a451903a11bd5f6f84e37421ab498234f
Henrique Saraiva <henriquesaraiva24@gmail.com>
2021-11-30 10:29:32 -0300

    Build Header Structure and Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook Clone! Let the game begin!</h1>
+      <Header />
     </div>
   );
 }
